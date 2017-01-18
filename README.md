caec26f4f0fd6d270531ecd3dbfa1854b266b9c6
rattlesnake <ricky.ratnayake@gmail.com>
2017-01-18 21:44:23 +1100

    removed sensistive data and added patient hashes

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # medical-project
 
 To Do >>
-- Remove 'test' rows
-- Mapping between patient(first, last, dob) => patient_hash
-- Mapping between UUID => patient_hash
+v Remove 'test' rows
+v Mapping between patient(first, last, dob) => patient_hash
+v Mapping between UUID => patient_hash
 
 - Mapping of (finalFile, field) to (File, column name)
 - Merge ? (delimit with |)
@@ -14,8 +14,7 @@ To Do >>
 
 
 
-
-()(()()())
+-----------------------
 what i did -
 
 removed 2 rows of testing data = new total matched rows = 25827
--- generate_patient_hash.py
+++ generate_patient_hash.py
@@ -1,4 +1,5 @@
 import csv
+import hashlib
 
 input_file = './csv/patient_data_ids.csv'
 dupe_maps_file = './csv/dupe-merge/all.csv'
@@ -24,6 +25,8 @@ with open(dupe_maps_file,
                                    if type(x) is bytes else x
                                    for x in row[1:4]).casefold()
         uuids = [x.strip().casefold() for x in row[0].split(':')]
+        if concat_pid_input == '':
+            raise ValueError('no pid or uuids - row - {}'.format(row))
         dupe_map[concat_pid_input] = uuids
 
 pid_to_uuids_map = {}
@@ -42,6 +45,8 @@ with open(input_file,
                              if type(x) is bytes else x
                              for x in row[0:3]).casefold()
         uuid = str(row[9]).casefold()
+        if concat_pid == '' or len(uuids) == 0:
+            raise ValueError('no pid or uuids - row - {}'.format(row))
         if concat_pid in dupe_map:
             pid_to_uuids_map[concat_pid] = dupe_map[concat_pid]
         else:
@@ -59,7 +64,10 @@ with open(uuid_to_hashed_pid_file,
     writer.writerow(['graphite_uuid', 'hashed_pid', 'pid'])
 
     for (pid, uuids) in pid_to_uuids_map.items():
-        rows = [(uuid, abs(hash(pid)), pid)
+        hash_algo = hashlib.sha256()
+        hash_algo.update(bytes(pid, 'utf-8'))
+        hashed_pid = hash_algo.hexdigest()
+        rows = [(uuid, hashed_pid, pid)
                 for uuid in uuids]
         writer.writerows(rows)
 
